ef042c4a3038b881f7394e2d5b3499769343d5c3
AnimXer01 <157260909+AnimXer01@users.noreply.github.com>
2024-03-13 17:13:28 +0800

    Update index.ts

--- config/index.ts
+++ config/index.ts
@@ -4,7 +4,7 @@ const isProduction = process.env.NODE_ENV === "production"
 
 export const config = {
     baseUrl: isProduction ? process.env.NEXT_PUBLIC_URL : 'http://localhost:3000',
-    tmdb_access_key: process.env.NEXT_PUBLIC_TMD_ACCESS_KEY,
+    tmdb_access_key: process.env.NEXT_PUBLIC_TMD_ACCESS_KEY : 'f7148ae89d27521674f16516d92bc883',
     disqus_shortname: process.env.NEXT_PUBLIC_DISQUS_SHORT_NAME,
     gogo: new ANIME.Gogoanime(),
     zoro: new ANIME.Zoro(),
